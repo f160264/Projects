e2e482b9557d2cede09de792d82ce771ecd15c9d
Torbjørn R Steinholm <Torbjrn R Steinholm@DESKTOP-UBVETK9>
2019-08-27 16:06:47 +0200

    b

---
+++ DAT-100 21 & 23.08.2019/no/hvl/dat100/Oppg1.java
@@ -0,0 +1,41 @@
+package no.hvl.dat100;
+
+import java.util.*;
+
+public class Oppg1 {
+
+	public static void main(String[] args) {
+
+		//Scanner sc = new Scanner(System.in);
+		//System.out.print("Write in the first number ");
+		//String numb1 = sc.next();
+		//System.out.print("Write in the second number ");
+		//String numb2 = sc.next();
+
+		//double nr1 = Integer.parseInt(numb1);
+		//double nr2 = Integer.parseInt(numb2);
+		
+		//System.out.println("The answer is: " + (nr1 / nr2));
+		//if (nr2 == 0) {
+		//System.out.print(" You can not divide by 0");
+		//}
+		
+		try {
+		double a = 10;
+		double b = 10; 
+		System.out.print(a/b);
+		}
+		catch( Exception e) {
+		System.out.println(e);
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
